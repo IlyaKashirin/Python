da8c2daac90618f2ba82e6b6a0701ea4df0d3427
IlyaKashirin <86298938+IlyaKashirin@users.noreply.github.com>
2023-12-16 15:06:23 +0300

    Add files via upload

---
+++ ДЗ_3/DZ_3_1.py
@@ -0,0 +1,14 @@
+
+def sum_distance(from1,to):
+    sum = 0
+    if from1 > to:
+        for i in range(to,from1+1):
+        sum = sum + i
+    print(sum)
+    elif:
+        for i in range(from1,to+1)
+        sum = sum + i
+    print(sum)
+sum_distance(10,1)
+
+
